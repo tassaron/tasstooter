2b0982f2f4617176c58ebd92d2ae2a6fd2906b3a
tassaron <rutachigal@gmail.com>
2018-08-23 21:38:54 -0400

    basic source-replying bot

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 """
 tasstooter, a simple program for organizing toots & tooting them later
 """
-from mastodon import Mastodon
+from mastodon import Mastodon, StreamListener
 import configparser
 import os
 import random
@@ -10,7 +10,7 @@ import argparse
 from string import ascii_letters
 
 
-class TootArchive(configparser.ConfigParser):
+class SavableConfigParser(configparser.ConfigParser):
     def __init__(self):
         super().__init__(interpolation=None)
         self.optionxform = str
@@ -30,17 +30,41 @@ class TootArchive(configparser.ConfigParser):
                 os.makedirs(configPath)
             return configPath
 
-        # load archive or make a new empty text file if none exists
-        configPath = makeConfigPath()
-        self.path = os.path.join(configPath, "tootarchive.ini")
-        if not os.path.exists(self.path):
-            with open(self.path, 'w') as f:
-                f.write('[DEFAULT]\nused = 0\nsource = 0\n\n[LASTTOOT]')
-        self.read(self.path)
+        self.configPath = makeConfigPath()
 
     def save(self):
         with open(self.path, 'w') as f:
             self.write(f)
+
+
+class SourceArchive(SavableConfigParser):
+    def __init__(self):
+        super().__init__()
+
+        # load archive or make a new empty text file if none exists
+        self.path = os.path.join(self.configPath, "sourcearchive.ini")
+        if not os.path.exists(self.path):
+            with open(self.path, 'w') as f:
+                f.write('[DEFAULT]\nused = 0')
+        self.read(self.path)
+
+    def add(self, tootId, source):
+        tootId = str(tootId)
+        self.add_section(tootId)
+        self[tootId]['source'] = source
+        self.save()
+
+
+class TootArchive(SavableConfigParser):
+    def __init__(self):
+        super().__init__()
+
+        # load archive or make a new empty text file if none exists
+        self.path = os.path.join(self.configPath, "tootarchive.ini")
+        if not os.path.exists(self.path):
+            with open(self.path, 'w') as f:
+                f.write('[DEFAULT]\nused = 0\nsource = 0\n\n[LASTTOOT]')
+        self.read(self.path)
 
     def add(self, toot, source=None):
         # create a new section in the ini file
@@ -73,13 +97,77 @@ class TootArchive(configparser.ConfigParser):
         return None
 
 
+class ReplyBot(StreamListener):
+    def __init__(self, mastodon, srcArchive):
+        super().__init__()
+        self.mastodon = mastodon
+        self.archive = srcArchive
+
+    def on_notification(self, notification):
+        acct = notification.get("account").get("acct")
+        notif_type = notification.get("type")
+        if notif_type == "reblog":
+            print("boosted by %s" % acct)
+            return
+        elif notif_type == "favourite":
+            print("favourited by %s" % acct)
+            return
+        elif notif_type == "follow":
+            print("new follower: %s" % acct)
+            return
+        elif notif_type != "mention":
+            print("unknown notification.")
+            return
+
+        status = notification.get("status")
+        if status is None:
+            print("status was none.")
+            return
+        content = cleanup(status.get("content"))
+        if content is None:
+            print("content was none.")
+            return
+        elif "source" not in content:
+            print("reply from %s: %s" % (acct, content))
+            return
+
+        # finally, toot the source if we can!
+        target = status.get("in_reply_to_id")
+        try:
+            if self.archive[str(target)]['used'] == '0':
+                url = self.archive[str(target)]['source']
+                self.mastodon.status_post(
+                    "@%s %s" % (acct, url), in_reply_to_id=status
+                )
+                self.archive[str(target)]['used'] = '1'
+                self.archive.save()
+        except KeyError:
+            print("reply to an id not found in archive: %s" % str(target))
+            return
+
+
+def cleanup(content):
+    if content is None:
+        return None
+    # there must be a better way to de-HTML this string...
+    content = content.replace("<p>", "").replace("</p>", "")
+    content = content.replace("&apos;", "'").split()
+    content = (
+        ' '.join(
+            [word.lower() for word in content if
+                set(word).isdisjoint(set("@<>\""))]
+        )
+    )
+    return content
+
+
 def login():
     mastodon = Mastodon(
         client_id='tasstooter_clientcred.secret',
         api_base_url='https://botsin.space'
     )
     mastodon.log_in(
-        'email,
+        'email',
         'password',
         to_file='tasstooter_usercred.secret'
     )
@@ -119,13 +207,19 @@ def main():
         help='toot a toot from archive with different source than last toot',
         action='store_true'
     )
+    parser.add_argument(
+        '-r', '--reply',
+        help='enter reply mode to respond to requests for source url',
+        action='store_true'
+    )
     arg = parser.parse_args()
 
     if arg.login:
         login()
 
-    if arg.add or arg.toot or arg.insert:
+    if arg.add or arg.toot or arg.insert or arg.reply:
         archive = TootArchive()
+        srcArchive = SourceArchive()
 
     if arg.insert:
         with open(arg.insert, 'r') as f:
@@ -138,14 +232,28 @@ def main():
     if arg.add:
         archive.add(arg.add[0], arg.add[1])
 
+    # now things that require connecting to an instance
+    mastodon = None
     if arg.toot:
         newToot = archive.fetch()
         if newToot:
             mastodon = connect()
             print('tooting: %s' % newToot['toot'])
-            mastodon.toot(newToot['toot'])
+            t = mastodon.toot(newToot['toot'])
+            newTootId = t.get("id")
+            srcArchive.add(newTootId, newToot['source'])
         else:
             print('No toots in the archive')
+
+    if arg.reply:
+        if mastodon is None:
+            mastodon = connect()
+        print("now in reply mode! press ctrl+c to stop")
+        try:
+            bot = ReplyBot(mastodon, srcArchive)
+            mastodon.stream_user(listener=bot)
+        except KeyboardInterrupt:
+            print("interrupt received, signing off...")
 
 
 if __name__ == "__main__":
